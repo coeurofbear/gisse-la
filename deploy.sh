d7b10a14750fc5e81fda2215a15842c6dfa3f612
Gíssela Reyes <coeurofbear@gmail.com>
2022-01-20 00:54:18 +0100

    feat: branch name

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:coeurofbear/gisse-la.git master:gh-pages
+git push -f git@github.com:coeurofbear/gisse-la.git main:gh-pages
 cd -
