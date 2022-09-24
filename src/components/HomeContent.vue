1d298e180b6fcb6390c18bb21959904b2b784420
Gíssela Reyes <coeurofbear@gmail.com>
2022-09-24 17:35:54 +0200

    feat: add css in the right places

--- src/components/HomeContent.vue
+++ src/components/HomeContent.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="wrapper">
     <h1>This is the beginning of my portfolio.</h1>
+    <!-- <div class="container">
+      <div class="row">
+        <div class="col-12 col-xl-6">
+          hola
+        </div>
+        <div class="col-12 col-xl-6">
+          hola
+        </div>
+      </div>
+    </div> -->
     <div class="bottom">
       <router-link to="/projects">Go to Projects</router-link>
     </div>
@@ -8,12 +18,16 @@
 </template>
 
 <style scoped lang="scss">
-@import '~@/assets/css/all.scss';
+@import '@/assets/css/variables/all.scss';
 
 h1 {
   font-size: 120px;
   color: white;
   margin-bottom: 40px;
+
+  @media #{$breakpoint-xl-down} {
+    font-size: 80px;
+  }
 }
 a {
   font-size: 25px;
