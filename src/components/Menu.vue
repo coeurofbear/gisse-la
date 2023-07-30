5dfc2eff64e874417ebd54de313ded5ef094a936
Gíssela Reyes <coeurofbear@gmail.com>
2023-07-30 09:47:10 +0200

    feat: menu sandwich

---
+++ src/components/Menu.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="top-bar">
+    <Logo v-if="false" />
+    <SandwichMenuIcon class="wrap" />
+  </div>
+</template>
+
+<script>
+import SandwichMenuIcon from './SandwichMenuIcon.vue'
+import Logo from './Logo.vue'
+
+export default {
+  components: {
+    SandwichMenuIcon,
+    Logo
+}
+}
+</script>
+<style scoped lang="scss">
+.wrap {
+  padding: 15px 0;
+  position: absolute;
+  right: 30px;
+  top: 50%;
+  transform: translateY(-50%);
+}
+.top-bar {
+  position: fixed;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  padding: 35px 0;
+}
+</style>
---
+++ src/components/SandwichMenuIcon.vue
@@ -0,0 +1,56 @@
+<template>
+  <div @click.native="isOpen = !isOpen;">
+    <div class="line" :class="{open: isOpen}"/>
+    <div class="line" :class="{open: isOpen}"/>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      isOpen: false
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+@import '@/assets/css/variables/all.scss';
+.line {
+  background: $cream;
+  height: 3px;
+  border-radius: 5px;
+  width: 40px;
+  transition: all .25s ease;
+
+  &:nth-child(1) {
+    margin-bottom: 5px;
+    transform: rotate(0);
+  }
+  &:nth-child(2) {
+    transform: rotate(0);
+  }
+
+  &.open {
+    &:nth-child(1) {
+      transform: rotate(45deg);
+      margin-bottom: -3px;
+    }
+    &:nth-child(2) {
+      transform: rotate(-45deg);
+    }
+  }
+}
+
+.open {
+  .line {
+    
+    &:nth-child(1) {
+      transform: rotate(45deg);
+    }
+    &:nth-child(2) {
+      transform: rotate(-45deg);
+    }
+  }
+}
+</style>
