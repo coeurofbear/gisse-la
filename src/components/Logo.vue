b647384f300d203dc4e16b6739098307df120fe2
Gíssela Reyes <coeurofbear@gmail.com>
2023-07-30 09:47:29 +0200

    feat: add logo and animate

---
+++ src/components/Logo.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <img id="logo-menu" class="logo-menu" src="@/assets/images/logo-full.svg" alt="">
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    height: {
+      type: Number,
+      default: 100
+    }
+  },
+  mounted() {
+    const logo = document.getElementById('logo-menu')
+    logo.style.setProperty('--height', `${this.height}px`)
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.logo-menu {
+  height: var(--height);
+}
+</style>
