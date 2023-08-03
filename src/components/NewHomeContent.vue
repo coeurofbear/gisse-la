1652018baca3ecd192b4e6cf94e824aa34d4632e
Gíssela Reyes <coeurofbear@gmail.com>
2023-08-04 00:54:04 +0200

    feat: copy

--- src/components/NewHomeContent.vue
+++ src/components/NewHomeContent.vue
@@ -8,7 +8,7 @@
           <p class="large">I'm a Front-End developer and Designer from Chile based in Berlin.</p>
           <p>I have more than 4 years of experience developing in Vue.js and recently started learning Flutter. I have experience in Graphic and Web Design, UI/UX and branding too. Although Graphic Design is not my main focus anymore, I still consider myself one :).</p>
           <p>In my free time I like to ride my road bike, take analogue photos and learn german.</p>
-          <p>Wanna know more about me?</p>
+          <p>Wanna know more?</p>
           <router-link class="btn" to="/about">About me</router-link>
         </div>
       </div>
